f08605dbcde4b31867c9f31cc83eefcf6fbf9d8d
chatlapin <choh9323@gmail.com>
2024-09-06 13:20:53 +0200

    outlet for header&footer

--- SportSee/src/pages/root.jsx
+++ SportSee/src/pages/root.jsx
@@ -1,10 +1,12 @@
 import Footer from '../composants/footer'
 import Header from '../composants/header'
+import {Outlet} from 'react-router-dom'
 
 export default function Root() {
   return (
     <>
-    <Header />
+    <Header className="container"/>
+    <Outlet/>
     <main>
 
     </main>
