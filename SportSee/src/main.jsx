5607c87fc205b3bfe780f4276cbb1d9ff29f9ccc
chatlapin <choh9323@gmail.com>
2024-09-06 13:34:20 +0200

    create BrowserRouter

--- SportSee/src/main.jsx
+++ SportSee/src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Home from './pages/home'
+import About from './pages/about'
+import Logement from './pages/logement'
+import './index.scss'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+        element: <Root />,
+        children: [
+            {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/logement/:id",
+    element: <Logement />,
+},
+      ]
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
