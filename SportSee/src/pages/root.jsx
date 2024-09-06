1bdeead29fa7d810c77d8d952200a1ee49e72c18
chatlapin <choh9323@gmail.com>
2024-09-06 13:06:29 +0200

    import Header and Footer & write Root function

---
+++ SportSee/src/pages/root.jsx
@@ -0,0 +1,14 @@
+import Footer from '../composants/footer'
+import Header from '../composants/header'
+
+export default function Root() {
+  return (
+    <>
+    <Header />
+    <main>
+
+    </main>
+    <Footer/>
+    </>
+  )
+}
