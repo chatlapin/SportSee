e91601b24a16b873a98fe7f774753e3cfbab6adf
chatlapin <choh9323@gmail.com>
2024-10-17 21:58:20 +0200

    rechart score (réunion le 17 oct)

--- src/composants/score.jsx
+++ src/composants/score.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
 import useFetch from '@/services/useFetch';
+import { json } from 'react-router-dom';
 
 const style = {
     top: '50%',
@@ -10,8 +11,10 @@ const style = {
 };
 
 export default function Example({ currentId }) {
-    const url = `http://localhost:3000/user/${currentId}/performance/`; // replace with your API endpoint
+    const url = `http://localhost:3000/user/${currentId}/`; // replace with your API endpoint
     const { data, loading, error } = useFetch(url);
+    console.log("score")
+    console.log(data)
 
     if (loading) {
         return <div>Loading...</div>;
@@ -21,16 +24,21 @@ export default function Example({ currentId }) {
         return <div>Error: {error.message}</div>;
     }
 
+    const formattedData = [{ score: (data.data.score || data.data.todayScore) }, { score: 1, fill: 'transparent' }];
+
     return (
-        <div className="tinyRadialChart">
-            <ResponsiveContainer width={300} height={100}>
-                <RadialBarChart width={300} height={100} cx="50%" cy="50%" innerRadius="30%" outerRadius="80%" barSize={10} data={data}>
+        <div style={{ width: '200px', height: '200px', backgroundColor: 'black', padding: '20px' }}>
+            <ResponsiveContainer width="100%" height="100%">
+                <RadialBarChart cx="50%" cy="50%" innerRadius="70%"
+                    outerRadius="100%" barSize={10} data={formattedData}
+                    startAngle={90} endAngle={450}
+                >
                     <RadialBar
-                        minAngle={15}
-                        label={{ position: 'insideStart', fill: '#fff' }}
-                        background
-                        clockWise
-                        dataKey="uv"
+                        clockWise="false"
+                        dataKey="score"
+                        fill='#ff0000'
+                        stroke-linejoin="round"
+                        cornerRadius={10}
                     />
                     <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
                 </RadialBarChart>
