fad395620d4d3ad1ba1b1093a1b9c164d61db0d5
chatlapin <choh9323@gmail.com>
2024-12-26 10:47:39 +0100

    yarn yarn dev lsof -i :3000 kill <PID>

---
+++ Back/app/routes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const idx = require("idx");
+
+const router = express.Router();
+
+const {
+  getUserById,
+  getUserActivityById,
+  getUserAverageSession,
+  getUserPerformance,
+} = require("./models");
+
+const { handleNoUserData } = require("./middleware");
+
+// Route handler for the root URL ("/")
+router.get("/", (req, res) => {
+  // Send a basic response indicating the server is running
+  res.send("Server is running!");
+});
+
+router.get("/user/:id", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserById(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+router.get("/user/:id/activity", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserActivityById(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+router.get("/user/:id/average-sessions", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserAverageSession(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+router.get("/user/:id/performance", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserPerformance(Number(userId));
+
+  return handleNoUserData(res, userData);
+});
+
+module.exports = router;
